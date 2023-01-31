59d6b149986bd2f0f8edd660b6b00fea22bb71f2
cstria0106 <cstria0106@gmail.com>
2023-02-01 00:12:40 +0900

    wip

--- client/fs/time_logger.go
+++ client/fs/time_logger.go
@@ -1,12 +1,29 @@
 package fs
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+)
 
 type timeLoggerKey struct{}
 
-type TimeLogger struct{}
+type TimeLogger struct {
+	file io.Writer
+}
 
-func WithTimeLogger(ctx context.Context, filepath string) context.Context {}
+func WithTimeLogger(ctx context.Context, filepath string) context.Context {
+	var file io.Writer
+	var err error
+	if file, err = os.Create(filepath); err != nil {
+		panic(fmt.Sprintf("file to create time logger file on %s", filepath))
+	}
+
+	return context.WithValue(ctx, timeLoggerKey{}, &TimeLogger{
+		file: file,
+	})
+}
 
 func GetTimeLogger(ctx context.Context) *TimeLogger {
 	l := ctx.Value(timeLoggerKey{})
