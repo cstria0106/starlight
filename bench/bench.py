a2f6b3ba9375e71357e1325d9143d1e3d3104710
cstria0106 <cstria0106@gmail.com>
2023-02-13 17:44:48 +0900

    update

--- bench/bench.py
+++ bench/bench.py
@@ -42,7 +42,7 @@ class TimerContext:
               (self.name, elapsed))
 
         if self.file is not None:
-            self.file.write('%f,%f' % (self.start_time, elapsed))
+            self.file.write('%f,%f\n' % (self.start_time, elapsed))
 
     def stop(self):
         if self.file is not None:
